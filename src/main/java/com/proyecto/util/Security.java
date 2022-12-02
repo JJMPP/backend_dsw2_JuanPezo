2c70c189e01131aa6b6c76037178c8224cdb48fc
JJMPP <juanjosemiguelp@gmail.com>
2022-12-02 05:10:38 -0500

    Proyecto Finalizado

--- src/main/java/com/proyecto/util/Security.java
+++ src/main/java/com/proyecto/util/Security.java
@@ -6,7 +6,7 @@ public class Security {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String JWT_TOKEN_HEADER = "Jwt-Token";
 	public static final String TOKEN_CANNOT_BE_VERIFIED = "Token no puede ser verificado";
-	public static final String GRUPO_02 = "Grupo 02";
+	public static final String GRUPO_04 = "Grupo 04";
 	public static final String AUTHORITIES = "authorities";
 	public static final String FORBIDDEN_MESSAGE = "Debes iniciar sesión para acceder a esta página";
 	public static final String ACCESS_DENIED_MESSAGE = "Usted no tiene permiso para acceder a esta página";
